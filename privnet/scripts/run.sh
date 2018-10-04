5c28d32671788da23255d37a426a76ff33832b37
Nuno Justo <nuno@justoweb.com>
2018-10-04 22:09:16 +0100

    ENV var and Case switch to open the correct wallet

--- privnet/scripts/run.sh
+++ privnet/scripts/run.sh
@@ -2,11 +2,26 @@
 #
 # This script starts four consensus and waits forever
 #
+echo $NODENBR
+echo
 
-screen -dmS node1 expect /opt/start_consensus_node.sh /opt/node/neo-cli/ wallet1.json one
-#screen -dmS node2 expect /opt/start_consensus_node.sh /opt/node2/neo-cli/ wallet2.json two
-#screen -dmS node3 expect /opt/start_consensus_node.sh /opt/node3/neo-cli/ wallet3.json three
-#screen -dmS node4 expect /opt/start_consensus_node.sh /opt/node4/neo-cli/ wallet4.json four
+case $NODENBR in
+1)
+  screen -dmS node1 expect /opt/start_consensus_node.sh /opt/node/neo-cli/ wallet1.json one
+  ;;
+2)
+  screen -dmS node2 expect /opt/start_consensus_node.sh /opt/node2/neo-cli/ wallet2.json two
+  ;;
+3)
+  screen -dmS node3 expect /opt/start_consensus_node.sh /opt/node3/neo-cli/ wallet3.json three
+  ;;
+4)
+  screen -dmS node4 expect /opt/start_consensus_node.sh /opt/node4/neo-cli/ wallet4.json four
+  ;;
+*)
+  Message="No NODENBR environment variable !!"
+  ;;
+esac
 
 sleep infinity
 
