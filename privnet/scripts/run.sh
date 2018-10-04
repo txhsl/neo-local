9c11d7af35c5ee128540ee34231f31bdd34cbf8f
Nuno Justo <nuno@justoweb.com>
2018-10-04 23:42:25 +0100

    Corrected dnpath for node2 to 4

--- privnet/scripts/run.sh
+++ privnet/scripts/run.sh
@@ -5,21 +5,26 @@
 echo $NODENBR
 echo
 
-case $NODENBR in
+case "$NODENBR" in
 1)
+  echo "case $NODENBR"
   screen -dmS node1 expect /opt/start_consensus_node.sh /opt/node/neo-cli/ wallet1.json one
   ;;
 2)
-  screen -dmS node2 expect /opt/start_consensus_node.sh /opt/node2/neo-cli/ wallet2.json two
+  echo "case $NODENBR"
+  screen -dmS node2 expect /opt/start_consensus_node.sh /opt/node/neo-cli/ wallet2.json two
   ;;
 3)
-  screen -dmS node3 expect /opt/start_consensus_node.sh /opt/node3/neo-cli/ wallet3.json three
+  echo "case $NODENBR"
+  screen -dmS node3 expect /opt/start_consensus_node.sh /opt/node/neo-cli/ wallet3.json three
   ;;
 4)
-  screen -dmS node4 expect /opt/start_consensus_node.sh /opt/node4/neo-cli/ wallet4.json four
+  echo "case $NODENBR"
+  screen -dmS node4 expect /opt/start_consensus_node.sh /opt/node/neo-cli/ wallet4.json four
   ;;
 *)
   Message="No NODENBR environment variable !!"
+  echo $Message
   ;;
 esac
 
